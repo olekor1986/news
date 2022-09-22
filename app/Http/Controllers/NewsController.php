b437e0086029593104df873b55e528b824cbe60a
olekor1986 <olekor1986@gmail.com>
2022-09-22 07:06:57 +0300

    create api routes and CRUD methods for News

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\News;
 
 class NewsController extends Controller
 {
-    //
+    public function news()
+    {
+        return response()->json(News::get(), 200);
+    }
+
+    public function newsById($id)
+    {
+        return response()->json(News::find($id), 200);
+    }
+
+    public function newsStore(Request $request)
+    {
+        $news = News::create($request->all());
+        return response()->json($news, 201);
+    }
+
+    public function newsUpdate(Request $request, News $news)
+    {
+        $news->update($request->all());
+        return response()->json($news, 200);
+    }
+
+    public function newsDestroy(News $news)
+    {
+        $news->delete();
+        return response()->json('', 204); 
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('news', 'NewsController@news');
+Route::get('news/{id}', 'NewsController@newsById');
+Route::post('news', 'NewsController@newsStore');
+Route::put('news/{news}', 'NewsController@newsUpdate');
+Route::delete('news/{news}', 'NewsController@newsDestroy');
